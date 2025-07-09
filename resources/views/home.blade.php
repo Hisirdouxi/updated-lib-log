04e55ca9a5f1b096cd50af7b00dd114fb689792d
rtadev15 <rtadev15@gmail.com>
2025-07-09 16:32:01 +0530

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <div class="space-y-6">
+        <section>
+            <h3 class="mb-5 font-bold">Top Books</h3>
+        
+            <div class="grid lg:grid-cols-2 gap-6">
+                <x-topbooks />
+                <x-topbooks />
+                <x-topbooks />
+                <x-topbooks />
+            </div>
+        </section>
+    </div>
+
+</x-layout>
