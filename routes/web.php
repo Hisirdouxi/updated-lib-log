288e6c6b75694ebbe7f32386459d2dc563000520
lalhriatzela <medium15432@gmail.com>
2025-07-10 22:22:06 +0530

    library login layout update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Authcontroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+
+Route::get('/login', function () {
+    return view('login');
+}
+);
